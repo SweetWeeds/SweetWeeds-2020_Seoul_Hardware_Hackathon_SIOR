ec47905ea2ffb0d45cc993d85c6b6a901d629986
Ubuntu <sior@SIOR.q04yglde2svefecrrpikaazgog.psx.internal.cloudapp.net>
2019-12-22 07:08:32 +0000

    뷰 테스트

--- sior/sior/settings.py
+++ sior/sior/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '##onj2j^*yr6@cp!@unnp=s@w9prt=w%+y-$@^ygh)0==6*v#g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
