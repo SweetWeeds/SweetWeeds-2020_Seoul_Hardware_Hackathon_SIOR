f6bfc0701d6e0c42216d3bdc89587f726654aa6e
Ubuntu <sior@SIOR.q04yglde2svefecrrpikaazgog.psx.internal.cloudapp.net>
2019-12-22 09:34:50 +0000

    로그인 구현

--- sior/sior/urls.py
+++ sior/sior/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import hat.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hat.views.login, name='login')
 ]
