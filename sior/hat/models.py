445eb3717c5cb2e0aa624d7f7099e3e6bcc47d5e
Ubuntu <sior@SIOR.q04yglde2svefecrrpikaazgog.psx.internal.cloudapp.net>
2019-12-25 12:30:08 +0000

    DB 수정

--- sior/hat/models.py
+++ sior/hat/models.py
@@ -7,10 +7,13 @@ MAX_LEN = 100
 class Hat(models.Model):
     id = models.AutoField(primary_key = True)
     user_name = models.CharField(max_length = 50)
-    gps = models.CharField(max_length = MAX_LEN, null = True, blank = True)
-    temperature = models.IntegerField(null = True, blank = True)
-    voc = models.IntegerField(null = True, blank = True)
-    tof = models.IntegerField(null = True, blank = True)
-    humid = models.IntegerField(null = True, blank = True)
-    gyro = models.CharField(max_length = MAX_LEN, null = True, blank = True)
     last_connect_date = models.DateTimeField(null = True, blank = True)
+
+class SensorVal(models.Model):
+    owner = models.ForeignKey(Hat, on_delete=models.CASCADE)        # 센서 값 소유자
+    recordtime = models.DateTimeField(auto_now_add=True)            # 센서 값 기록 시간
+    gps = models.CharField(max_length = MAX_LEN, null = True, blank = True) # GPS값
+    temperature = models.IntegerField(null = True, blank = True)    # 온도 값
+    voc = models.IntegerField(null = True, blank = True)            # voc 값
+    humid = models.IntegerField(null = True, blank = True)          # 습도 값
+    gyro = models.CharField(max_length = MAX_LEN, null = True, blank = True)    # 자이로 센서 값
