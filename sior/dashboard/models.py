c71f538969ac21967c48bf6a803b2fe278ef6077
Ubuntu <sior@SIOR.q04yglde2svefecrrpikaazgog.psx.internal.cloudapp.net>
2019-12-26 13:50:02 +0000

    191226

--- sior/dashboard/models.py
+++ sior/dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import DateTimeField
+from django.db.models import Max
 MAX_LEN = 100
 
 # Create your models here.
@@ -7,9 +8,11 @@ MAX_LEN = 100
 class Hat(models.Model):
     id = models.AutoField(primary_key = True)
     user_name = models.CharField(max_length = 50)
+    employee_id = models.CharField(max_length = 100, null = True)
     phone_number = models.CharField(max_length = 20, null = True)
     job_position = models.CharField(max_length = MAX_LEN, null = True)
-    last_location_gps = models.CharField(max_length = MAX_LEN, null = True, blank = True)
+    last_location_gps_lat = models.CharField(max_length = MAX_LEN, null = True, blank = True)
+    last_location_gps_lng = models.CharField(max_length = MAX_LEN, null = True, blank = True)
     last_location_str = models.CharField(max_length = MAX_LEN, null = True, blank = True)
     last_connect_date = models.DateTimeField(null = True, blank = True)
 
@@ -21,3 +24,7 @@ class SensorValue(models.Model):
     voc = models.IntegerField(null = True, blank = True)            # voc 값
     humid = models.IntegerField(null = True, blank = True)          # 습도 값
     gyro = models.CharField(max_length = MAX_LEN, null = True, blank = True)    # 자이로 센서 값
+
+
+
+#max_temperature = SensorValue.objects.all().aggregate(Max('temperature'))
--- sior/dashboard/views.py
+++ sior/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from .models import Hat
+from .models import Hat, SensorValue
+from django.db.models import Max
 
 # Create your views here.
 def isauth(request):
@@ -12,10 +13,12 @@ def isauth(request):
         return redirect('accounts/login')
 
 def home(request):
+    SensorValues = SensorValue.objects
     if not request.user.is_authenticated:
         return redirect('../accounts/login')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'SensorValues' : SensorValues})
+
 
 def location(request):
     Hats = Hat.objects
@@ -23,4 +26,27 @@ def location(request):
 
 
 def statistics(request):
-    return render(request, 'statistics.html', {})
+    SensorValues = SensorValue.objects
+    max_temperature = SensorValue.objects.all().aggregate(Max('temperature'))
+    return render(request, 'statistics.html', {'SensorValues' : SensorValues})
+
+''' 혁수가 보기 편할려고 추가함.
+    class Hat(models.Model):
+    id = models.AutoField(primary_key = True)
+    user_name = models.CharField(max_length = 50)
+    phone_number = models.CharField(max_length = 20, null = True)
+    job_position = models.CharField(max_length = MAX_LEN, null = True)
+    last_location_gps_lat = models.CharField(max_length = MAX_LEN, null = True, blank = True)
+    last_location_gps_lng = models.CharField(max_length = MAX_LEN, null = True, blank = True)
+    last_location_str = models.CharField(max_length = MAX_LEN, null = True, blank = True)
+    last_connect_date = models.DateTimeField(null = True, blank = True)
+
+    class SensorValue(models.Model):
+    owner = models.ForeignKey(Hat, on_delete=models.CASCADE)        # 센서 값 소유자
+    recordtime = models.DateTimeField(auto_now_add=True)            # 센서 값 기록 시간
+    gps = models.CharField(max_length = MAX_LEN, null = True, blank = True) # GPS값
+    temperature = models.IntegerField(null = True, blank = True)    # 온도 값
+    voc = models.IntegerField(null = True, blank = True)            # voc 값
+    humid = models.IntegerField(null = True, blank = True)          # 습도 값
+    gyro = models.CharField(max_length = MAX_LEN, null = True, blank = True)    # 자이로 센서 값
+'''
